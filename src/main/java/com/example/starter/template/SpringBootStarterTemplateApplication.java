2f30f6db96453978e70c9f0ebf0364da233f18a0
minsun24 <jms49526@gmail.com>
2026-05-07 23:10:57 +0900

    init: 보일러플레이트 구조 및 의존성 업데이트(Mariadb, Security)

---
+++ src/main/java/com/example/starter/template/SpringBootStarterTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.starter.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterTemplateApplication.class, args);
+    }
+
+}
